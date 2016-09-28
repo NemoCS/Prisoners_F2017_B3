e27d75573af19f95a5f809f51fa8ea3167272d5c
HelloKitty4Life <szewcandrew@gmail.com>
2016-09-28 12:54:35 -0500

    All Names

--- team6.py
+++ team6.py
@@ -1,5 +1,5 @@
 ####
-# Names: Andrew Szewc
+# Names: Andrew Szewc, Tanner Wong and Nolan Velazquez
 # Each team's file must define four tokens:
 #     team_name: a string
 #     strategy_name: a string
